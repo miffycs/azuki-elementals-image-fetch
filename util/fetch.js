79f80542d9297b011969a00e66befcbfa3f1df79
Miffy Chen <mchen15@bu.edu>
2023-11-27 13:32:53 -0500

    update API usage (param: skip & limit)
    team introduced pagination, limit capped at 1000, skip = starting index

--- util/fetch.js
+++ util/fetch.js
@@ -2,35 +2,62 @@ const https = require("https");
 const fs = require("fs");
 
 // Write to a file
-const writeFile = (filename, data) => new Promise((resolve, reject) => {
-    fs.writeFile(filename, data, (err) => {
+const writeFile = (filename, data, flag = 'w') => new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, { flag }, (err) => {
         if (err) reject(err);
         else resolve();
     });
 });
 
 // Fetch and save data as json
-const fetchData = () => new Promise((resolve, reject) => {
-    https.get("https://www.azuki.com/api/elemental_token_configs", (res) => {
-        let data = "";
-        res.on("data", (chunk) => data += chunk);
-        res.on("end", async () => {
-            const parsedData = JSON.parse(data);
-            const slugs = parsedData.ELEMENTALS.map(elemental => {
-                let obj = {};
-                obj[elemental.name.substring(11)] = elemental.image.substring(36, 72);
-                return obj;
-            });
-            try {
-                await writeFile("data.json", JSON.stringify(slugs, null, 2));
-                console.log("Data written to JSON file");
-                resolve();
-            } catch (err) {
+const fetchData = async () => {
+    const limit = 1000; // Max records per request
+    let skip = 0;       // Starting point for each batch
+    let hasMore = true; // Flag to indicate more data to fetch
+
+    let allSlugs = []; // Accumulate all slugs here
+
+    while (hasMore) {
+        const url = `https://www.azuki.com/api/elemental_token_configs?skip=${skip}&limit=${limit}`;
+        await new Promise((resolve, reject) => {
+            https.get(url, (res) => {
+                let data = "";
+                res.on("data", (chunk) => data += chunk);
+                res.on("end", () => {
+                    const parsedData = JSON.parse(data);
+
+                    if (parsedData.ELEMENTALS && parsedData.ELEMENTALS.length > 0) {
+                        parsedData.ELEMENTALS.forEach(elemental => {
+                            let obj = {};
+                            obj[elemental.name.substring(11)] = elemental.image.substring(36, 72);
+                            allSlugs.push(obj);
+                        });
+
+                        if (parsedData.ELEMENTALS.length < limit) {
+                            hasMore = false; // No more data to fetch
+                        }
+                    } else {
+                        hasMore = false; // No data in response
+                    }
+
+                    skip += limit; // Increment for next batch
+                    resolve();
+                });
+            }).on("error", (err) => {
+                console.error("Error during HTTP request:", err);
                 reject(err);
-            }
+            });
         });
-    }).on("error", reject);
-});
+    }
+
+    // Write accumulated data to file
+    try {
+        await writeFile("data.json", JSON.stringify(allSlugs, null, 2));
+        console.log("Data written to JSON file");
+    } catch (err) {
+        console.error("Error writing to file:", err);
+    }
+};
 
 // Convert json to js
 const convertJsonToJs = async () => {
